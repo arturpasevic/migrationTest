1e248ebd6f6e39183fae36214724007d6ae5e6be
Арчибальд Паштетовичъ <ar4ibaldo@gmail.com>
2019-05-31 12:12:02 +0300

    Add project files.

---
+++ migrationTest/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using migrationTest.Models;
+
+namespace migrationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return Json(await _context.Phones.ToListAsync());
+        }
+
+        public async Task Create()
+        {
+            await _context.Phones.AddAsync(new Phone { Company = "Sams", Name = "wth", Price = 10 });
+            await _context.Phones.AddAsync(new Phone { Company = "dkidkid", Name = "zxcvzxc", Price = 20 });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ migrationTest/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using migrationTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace migrationTest
+{
+    public class MyContext : DbContext
+    {
+
+        public DbSet<Phone> Phones { get; set; }
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
+        {
+        }
+    }
+}
